4b1916192de6f899f8a8b8c8c78c8ca910aa36c2
SUGATA LAHA <sugatalaha@gmail.com>
2026-05-19 23:25:49 +0530

    add select sys call for IO multiplexing

--- echo-client.py
+++ echo-client.py
@@ -1,4 +1,6 @@
 import socket
+import sys
+import select
 
 IP_CONNECT="127.0.0.1"
 PORT_CONNECT=7007
@@ -7,21 +9,26 @@ BUFFER_SIZE=65535
 def client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((IP_CONNECT, PORT_CONNECT))
+        rlist=[sys.stdin, sock]
         print("Connected to server...")
         name=str(input("Name: "))
         sock.send(f"name={name.strip()}".encode())
-        received_bytes=sock.recv(BUFFER_SIZE)
-        print(f"Recv: {received_bytes.decode()}")
-        while True:
-            command=str(input("Send: "))
-            if command.strip().lower()=="quit":
-                print("Connection closing...")
-                break
-            else:
-                data=command
-                sock.send(data.encode())
-                received=sock.recv(BUFFER_SIZE)
-                print(f"Recv: {received.decode()}")
-
+        break_connection=False
+        while not break_connection:
+            ready_fds, _, _=select.select(rlist, [], [])
+            for source in ready_fds:
+                if source==sock:
+                    received_bytes=sock.recv(BUFFER_SIZE)
+                    print(f"Recv: {received_bytes.decode()}")
+                else:
+                    command=sys.stdin.readline()
+                    if command.strip().lower()=="quit":
+                        print("Connection closing...")
+                        break_connection=True
+                        break
+                    else:
+                        data=command.strip()
+                        sock.send(data.encode())
+            
 if __name__=="__main__":
     client()
