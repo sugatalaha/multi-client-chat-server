1719067f94801e431044d0fb15dafceee67e9613
SUGATA LAHA <sugatalaha@gmail.com>
2026-05-25 17:36:53 +0530

    remove nested loop for collecting message

--- client.py
+++ client.py
@@ -8,20 +8,6 @@ BUFFER_SIZE=65535
 
 buffer=""
 
-def fill_buffer(sock):
-    global buffer
-    while True:
-        received_bytes=sock.recv(BUFFER_SIZE)
-        received_msg=received_bytes.decode()
-        if not received_msg:
-            return None
-        buffer+=received_msg
-        if "\n" in buffer:
-            total_message, remaining=buffer.split("\n", 1)
-            buffer=remaining
-            break
-    return total_message
-
 def client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((IP_CONNECT, PORT_CONNECT))
@@ -34,9 +20,14 @@ def client():
             ready_fds, _, _=select.select(rlist, [], [])
             for source in ready_fds:
                 if source==sock:
-                    received_message=fill_buffer(sock)
+                    received_bytes=sock.recv(BUFFER_SIZE)
+                    received_message=received_bytes.decode()
                     if received_message==None:
                         break
+                    global buffer
+                    buffer+=received_message
+                    if "\n" in buffer:
+                        received_message, buffer=buffer.split("\n", 1)
                     print(f"Recv: {received_message}")
                 else:
                     command=sys.stdin.readline()
